e6e4e5c6714a70d14b04f7f4baf24ff95547c3e4
Levy Mota <levy@iMac-de-Levy.local>
2019-11-16 15:30:27 -0300

    guarda de rotas

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -1,19 +1,22 @@
-import {Injectable} from "@angular/core";
-import {CanActivate, Router} from "@angular/router";
-import {AuthService} from "./auth.service";
+import { Injectable } from "@angular/core";
+import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from "@angular/router";
+import { AuthService } from "./auth.service";
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class AuthGuardService implements CanActivate {
 
-    constructor(private _authService: AuthService, private _router: Router){}
+    constructor(private _authService: AuthService, private _router: Router) { }
 
-    canActivate() {
-        if(this._authService.isLoggedIn()){
-            this._authService.refreshToken();
+    canActivate(
+        route: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot
+    ): Observable<boolean> | boolean {
+        if (this._authService.isLoggedIn()) {
             return true;
-        } else {
-            this._authService.logout();
-            this._router.navigate(['']);
         }
+        this._router.navigate(['/']);
+        return false;
+
     }
 }
